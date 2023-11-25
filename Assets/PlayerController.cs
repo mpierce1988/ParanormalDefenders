e44655c3ba2bcbab4125fc6c83bb61b1045856eb
michaelpierce <michael.todd.pierce@gmail.com>
2023-11-25 14:53:44 -0400

    Added SO for each input, changed PlayerController and Unity Player Input to use SO

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,40 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private ScriptableObjectArchitecture.Vector2Variable _movementInput;
+
+    [SerializeField]
     private float _movementSpeed = 30f;
 
-    private IPlayerInput _playerInput;
+    // private IPlayerInput _playerInput;
     private IMovement _playerMovement;
     private Rigidbody2D _rigidbody2D;
 
-    private Vector2 _movementVector;
 
     private void Awake()
     {
-        _playerInput = GetComponent<IPlayerInput>();
+        // _playerInput = GetComponent<IPlayerInput>();
         _playerMovement = GetComponent<IMovement>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-    // Start is called before the first frame update
-    void OnEnable()
-    {
-        _playerInput.OnMovementChange += UpdateMovementVector;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Debug.Log("Current Input: " + _movementInput.Value);
         _rigidbody2D.MovePosition(_playerMovement.GetNextPosition(
-            _rigidbody2D.position, _movementVector, Time.fixedDeltaTime));
+            _rigidbody2D.position, _movementInput.Value, Time.fixedDeltaTime));
     }
 
-    private void OnDisable()
-    {
-        _playerInput.OnMovementChange -= UpdateMovementVector;
-    }
-
-    private void UpdateMovementVector(Vector2 obj)
-    {
-        _movementVector = obj;
-    }
 }
