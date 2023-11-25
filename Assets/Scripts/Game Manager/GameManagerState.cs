b30aa1015a9b894f4ceea756b4d5ea4eae9b2d47
michaelpierce <michael.todd.pierce@gmail.com>
2023-11-25 18:26:33 -0400

    Implemented pause, GameManager with Play and Pause state

---
+++ Assets/Scripts/Game Manager/GameManagerState.cs
@@ -0,0 +1,13 @@
+﻿public abstract class GameManagerState
+{
+    protected GameManager gameManager;
+
+    public GameManagerState(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
+    public abstract void Enter();
+    public abstract void Exit();
+    public abstract void Update();
+}
---
+++ Assets/Scripts/Game Manager/PauseState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PauseState : GameManagerState
+{
+    public PauseState(GameManager gameManager) : base(gameManager)
+    {
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Game Manager entered Pause State");
+        Time.timeScale = 0f;
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Game Manager exited Pause State");
+        Time.timeScale = 1f;
+    }
+
+    public override void Update()
+    {
+
+    }
+}
