4cea8d8d7987610bccfd5b5ba3a5caa9385e80f7
michaelpierce <michael.todd.pierce@gmail.com>
2023-12-03 14:18:38 -0400

    Refactored Projectile into a base class PlayerSpawnable

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,68 +1,12 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class Projectile : MonoBehaviour
+
+public class Projectile : PlayerSpawnable
 {
-    [SerializeField]
-    private float _speed;
-
-    [SerializeField]
-    private int _damage;
-
-    [SerializeField]
-    private float _destroyAfterSeconds = -1f;
-
-    private Rigidbody2D _rigidbody2D;
-    private Vector2? _launchAngle = null;
-
-    private void Awake()
+    protected override void Launch()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void OnEnable()
-    {
-
-        _launchAngle = transform.right;
-
         _rigidbody2D.velocity = Vector2.zero;
-        Debug.Log("Launching projectile at angle: " + _launchAngle + ", localRotation: " + transform.rotation);
-        _rigidbody2D.AddForce(_launchAngle.Value * _speed, ForceMode2D.Impulse);
-
-        if (_destroyAfterSeconds > 0)
-        {
-            StartCoroutine(DestroyAfter(_destroyAfterSeconds));
-        }
-    }
-
-    IEnumerator DestroyAfter(float destroyAfterSeconds)
-    {
-        yield return new WaitForSeconds(destroyAfterSeconds);
-
-        Destroy(this.gameObject);
-    }
-
-    private void OnDisable()
-    {
-
-    }
-
-    public void SetDamage(int damage)
-    {
-        _damage = damage;
-    }
-
-    public void SetSpeed(int speed)
-    {
-        _speed = speed;
-    }
-
-    public void SetLaunchAngle(Vector2 launchAngle)
-    {
-        Debug.Log("Setting the launch angle to: " + launchAngle);
-        _launchAngle = launchAngle;
+        _rigidbody2D.AddForce(transform.right * _speed, ForceMode2D.Impulse);
     }
 }
